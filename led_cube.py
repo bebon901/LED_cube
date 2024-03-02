03d49aa3a320a6412684a2d138da52fc3d6e754b
Ben Benson <benbenson2004@gmail.com>
2024-03-02 12:31:57 +0000

    more cprogresss

--- led_cube.py
+++ led_cube.py
@@ -7,7 +7,7 @@ class cube:
         self.dim_x = x
         self.dim_y = y
         self.dim_z = z
-        self.pixels = neopixel.NeoPixel(board.D18, x*y*z, pixel_order=(0, 1, 2))
+        self.pixels = neopixel.NeoPixel(board.D18, x*y*z, pixel_order=(0, 1, 2), auto_write=False)
     def pixel(self, x, y, z, color):
         self.pixels[x*25+y*5+z] = color
     def show(self):
--- rainbow_cube.py
+++ rainbow_cube.py
@@ -8,6 +8,6 @@ while True:
         for x in range(5):
             for y in range(5):
                 for z in range(5):
-                    cube.pixel(x, y, z, (colorsys.hsv_to_rgb(z*5/255+rot/100, 1, 1)[0]*255, colorsys.hsv_to_rgb(z*5/100+rot/255, 1, 1)[1]*255, colorsys.hsv_to_rgb(z*5/255+rot/100, 1, 1)[2]*255))
-    time.sleep(0.0001)
-    cube.show()
+                    cube.pixel(x, y, z, tuple(i * 255 for i in colorsys.hsv_to_rgb(z*5/255+rot/100, 1-y/5, 1)))
+        time.sleep(0.05)
+        cube.show()
